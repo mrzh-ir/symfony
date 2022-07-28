40f02098a8d93d7eeb42ad802eba3d1fd75f37d9
unknown <m.iranshahi67@gmail.com>
2022-07-28 17:16:50 +0200

    symfony/twig-bundle install

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
-    #[Route(path:'/{firstName}', name:'app_home_index')]
-    public function index(string $firstName): Response {
-        return new Response("<h1>Hello $firstName</h1>");
+    #[Route(path:'/', name:'app_home_index')]
+    public function index(): Response {
+        return $this->render('home/index.html.twig');
     }
 }
