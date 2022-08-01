4f8f93d9174f4f35214272f1a4dd891577635f1c
unknown <m.iranshahi67@gmail.com>
2022-08-01 17:14:55 +0200

    link and path

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -9,12 +9,23 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route(path:'/{name}', name:'app_home_index')]
-    public function index(string $name): Response {
+    #[Route(path:'/', name:'app_home_index')]
+    public function index(): Response {
 
-        $person = new Person($name);
+        $person = new Person("lena");
 
         return $this->render('home/index.html.twig', [
+            'model' => $person,
+        ]);
+    }
+
+
+    #[Route(path:'/overview', name:'app_home_overview')]
+    public function overview(): Response {
+
+        $person = new Person("lena");
+
+        return $this->render('home/overview.html.twig', [
             'model' => $person,
         ]);
     }
