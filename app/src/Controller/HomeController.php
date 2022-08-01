0d54aa8c4477894e4689f4dfa074f0c0f7bfd549
unknown <m.iranshahi67@gmail.com>
2022-08-01 14:08:34 +0200

    twig debugging

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Model\Person;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,8 +11,11 @@ class HomeController extends AbstractController
 {
     #[Route(path:'/{name}', name:'app_home_index')]
     public function index(string $name): Response {
+
+        $person = new Person($name);
+
         return $this->render('home/index.html.twig', [
-            'model' => $name,
+            'model' => $person,
         ]);
     }
 }
